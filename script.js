b247fd9767db5506c870efc1928a681fd8576776
Kasis Shahi <kasisshhahi@gmail.com>
2026-08-28 00:43:10 +0545

    little bit left

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
-const selectTag= document.querySelectorAll("select");
-const btn= document.querySelector("button");
-const fromText= document.querySelector(".from-text");
-const toText= document.querySelector(".to-text");
+const selectTag = document.querySelectorAll("select");
+const btn = document.querySelector("button");
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const restart = document.querySelector(".restart");
 
 
 
 
 //todo because of multiple querySelect so forEach is used
-selectTag.forEach((tag, index)=>{
+selectTag.forEach((tag, index) => {
 
     // todo as countries is an object 
-for(let code in countries){
-let option=document.createElement("option");
-option.textContent=countries[code];
+    for (let code in countries) {
+        let option = document.createElement("option");
+        option.textContent = countries[code];
 
-//todo having by default
-if(index==0){
-    if(option.textContent==="English"){
-        option.selected=true;
-    }
-}
-if(index==1){
-   if(option.textContent==="Nepali"){
-        option.selected=true;
-    } 
-}
+        //todo having by default
+        if (index == 0) {
+            if (option.textContent === "English") {
+                option.selected = true;
+            }
+        }
+        if (index == 1) {
+            if (option.textContent === "Nepali") {
+                option.selected = true;
+            }
+        }
 
-option.value=code;
-tag.append(option);
+        option.value = code;
+        tag.append(option);
     }
 });
 
-const translate= async()=>{
-   
-
-// todo fetching api
-const url=`https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
-const response= await fetch(url);
-const data=await response.json();
-console.log(data);
+//todo main api function 
+const translate = async (texts, translateFroms, translateTos) => {
+    // todo fetching api
+    const url = `https://api.mymemory.translated.net/get?q=${texts}&langpair=${translateFroms}|${translateTos}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    return data;
 
 
 };
-btn.addEventListener("click",()=>{
- let text=fromText.value;
+//todo to use btn
+btn.addEventListener("click", async () => {
+    let text = fromText.value;
     // console.log(text); 
-let translateFrom=selectTag[0].value;
-let translateTo=selectTag[1].value;
-// console.log(translateFrom,translateTo);
-translate();
-toText.textContent=data.matches[2].translation;
+    let translateFrom = selectTag[0].value;
+    let translateTo = selectTag[1].value;
+    // console.log(translateFrom,translateTo);
+    //todo translate function ko value use garna
+    // translate(text,translateFrom,translateTo);
+    //todo now i can use data
+    let data = await translate(text, translateFrom, translateTo);
+    toText.value = data.matches[2].translation;
 });
 
-selectTag[0].addEventListener("change",()=>{
+//todo while it is triggering
+selectTag[0].addEventListener("change", async () => {
+    let text = fromText.value;
+    // console.log(text); 
+    let translateFrom = selectTag[1].value;
+    let translateTo = selectTag[0].value;
 
-})
+
+    let data = await translate(text, translateFrom, translateTo);
+    console.log(data);
+    fromText.value = data.matches[0].translation;
+
+    
+
+   
+    
+
+
+});
+
 
